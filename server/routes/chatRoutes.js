7c25b0dd7add72dc36d563fffebd987803d7e35f
malo-dev <leadermushio377@gmail.com>
2022-11-23 15:29:24 +0200

    feat : createGroupChat routes

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import accessChat from '../controllers/accessChat.js';
+import createGroupChat from '../controllers/createGroupChat.js';
 import fetchChats from '../controllers/fetchChats.js';
 import protect from '../middlewares/authMiddler.js';
+
 const router = express.Router();
 router.route('/').post(protect, accessChat)
 router.route('/').get(protect, fetchChats)
-// router.route("/group").post(protect, createGroupChat)
+ router.route("/group").post(protect, createGroupChat)
 // router.route("/rename").put(protect, renameGroup)
 // router.route("/groupremove").put(protect,removeFromGroup)
 // router.route("/groupadd").put(protect,addToGroup)
