cdeb622f6191183e9ca2f65be7efab1b56ee9492
malo-dev <leadermushio377@gmail.com>
2022-11-23 15:49:03 +0200

    feat : renameGroup route

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -2,13 +2,14 @@ import express from 'express'
 import accessChat from '../controllers/accessChat.js';
 import createGroupChat from '../controllers/createGroupChat.js';
 import fetchChats from '../controllers/fetchChats.js';
+import renameGroup from '../controllers/renameGroup.js';
 import protect from '../middlewares/authMiddler.js';
 
 const router = express.Router();
 router.route('/').post(protect, accessChat)
 router.route('/').get(protect, fetchChats)
  router.route("/group").post(protect, createGroupChat)
-// router.route("/rename").put(protect, renameGroup)
-// router.route("/groupremove").put(protect,removeFromGroup)
-// router.route("/groupadd").put(protect,addToGroup)
+router.route("/rename").put(protect, renameGroup)
+// router.route("/groupremove").delete(protect,removeFromGroup)
+// router.route("/groupadd").post(protect,addToGroup)
 export default router
